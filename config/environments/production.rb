6bbfc98f50240eee4029b81a757f9be8800164fc
AlbertZasura <albertalbert841@gmail.com>
2021-02-24 18:14:07 +0700

    update email sendgrid && create relationship

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,8 +74,8 @@ Rails.application.configure do
   :address => 'smtp.sendgrid.net',
   :port => '587',
   :authentication => :plain,
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
+  :user_name => app198414064@heroku.com,
+  :password => khosvqbb3696,
   :domain => 'heroku.com',
   :enable_starttls_auto => true
   }
