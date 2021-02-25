8ecad9ee7366b59a8bf2fac5710bdf4ae04956de
AlbertZasura <albertalbert841@gmail.com>
2021-02-25 16:13:43 +0700

    change account sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,9 +73,9 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'heroku.com',
-    :address => 'smtp.sendgrid.net',
-    :port => '587',
+    :domain => 'ggwash.id',
+    :address => 'ggwash.id',
+    :port => '465',
     :authentication => :plain,
     :enable_starttls_auto => true
   }
