a7e41661434d4a0244b082159f14b53443c2b6b0
AlbertZasura <albertalbert841@gmail.com>
2021-02-25 18:06:36 +0700

    update account

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,9 +74,9 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'ggwash.id',
-    :address => 'ggwash.id',
-    :port => '465',
+    :domain => 'mailjet.com',
+    :address => 'in-v3.mailjet.com',
+    :port => '587',
     :authentication => :plain,
     :enable_starttls_auto => true,
     :openssl_verify_mode => 'none'
