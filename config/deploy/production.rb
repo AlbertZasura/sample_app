a2aed274236bf0afc077122305514e63732aade8
owner <albertalbert841@gmail.com>
2022-06-03 19:42:05 +0700

    Add: branch main

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@ server "159.65.135.84", user: "deploy", roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-
+set :branch, 'main'
 
 # role-based syntax
 # ==================
